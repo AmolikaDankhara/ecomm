2ca0f4f175e6d221c0959d8940c8f6b22e0435c5
AmolikaDankhara <amolikadankhara@gmail.com>
2025-08-24 20:26:38 +0530

    overley remove

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -26,7 +26,7 @@ const ProductCard = ({ product }) => {
                     src={product.image}
                     alt="product image"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-20" />
+                
             </div>
 
             <div className="mt-4 px-5 pb-5">
